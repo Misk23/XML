7875e39b7a6c8864c2cec6911a4b42f59ed91b09
Misk <mihajlo.jovkovic@gmail.com>
2020-08-07 19:58:45 +0200

    initial commit

---
+++ XML_tim31/src/main/java/com/xml/publications/PublicationsApplication.java
@@ -0,0 +1,16 @@
+package com.xml.publications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicationsApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(PublicationsApplication.class, args);
+
+        System.out.println("Hello world!");
+    }
+
+}
---
+++ XML_tim31/src/test/java/com/xml/publications/PublicationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xml.publications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
